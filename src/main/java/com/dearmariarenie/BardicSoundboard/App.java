3c91db40f84d89cedd0f6e9e6d758b8b792e9de9
Maria Garavaglia <maria.renie.garavaglia@gmail.com>
2025-08-15 15:23:32 -0400

    Removes old code for pre-loading audio/character files as resources

--- src/main/java/com/dearmariarenie/BardicSoundboard/App.java
+++ src/main/java/com/dearmariarenie/BardicSoundboard/App.java
@@ -16,69 +16,13 @@ public class App extends Application
     public static Config config;
 
 
-    public static void main(String[] args)// throws IOException
+    public static void main(String[] args)
     {
         config = new Config();
-
-//        findOrCreateHomeDirs();
 
         //Launch FX
         launch();
     }
-
-//    private static void findOrCreateHomeDirs() throws IOException
-//    {
-//        var dirNames = new String[]{
-//            CHARACTERS_DIR,
-//            AUDIO_DIR
-//        };
-//
-//        for(var dirName : dirNames)
-//        {
-//            var dir = new File(ROOT_DIR + dirName);
-//            // create if it doesn't exist yet
-//            if(!dir.exists())
-//            {
-//                System.out.println("Directory \"" + ROOT_DIR + dirName + "\" not found, creating new one");
-//
-//                if(dir.mkdirs())
-//                {
-//                    //Get all resource files in the given directory
-//                    //Trying to get the whole folder as a single resource just returns null, so we have to use reflection instead
-//                    ScanResult scan = new ClassGraph()
-//                        .acceptPaths(dirName)
-//                        .scan();
-//                    var files = scan.getAllResources().nonClassFilesOnly();
-//                    System.out.println("Found " + files.size() + " items");
-//
-//                    for(var file : files)
-//                    {
-//                        //get path name, removing the classpath tacked on the front
-//                        var classpath = file.getClasspathElementURI().getPath();
-//                        var filePath = file.getURI().getPath().replaceAll(classpath, "");
-//
-//                        //make a new path ROOT_DIR/dirName/filename.xyz to copy the file to
-//                        var filename = Paths.get(filePath).getFileName().toString();
-//                        var newPath = Paths.get(ROOT_DIR + dirName + File.separator + filename);
-//
-//                        //Copy the file
-//                        System.out.println("Copying file \"" + filePath + "\" to \"" + newPath + "\"");
-//                        Files.copy(App.class.getResourceAsStream(filePath), newPath);
-//                    }
-//                }
-//                else
-//                {
-//                    throw new IOException("Directory creation failed");
-//                }
-//            }
-//
-//            // make sure it's a directory
-//            if(!dir.isDirectory())
-//            {
-//                throw new IOException(ROOT_DIR + " is not a directory");
-//            }
-//        }
-//    }
 
     @Override
     public void start(Stage stage) throws IOException
